cf0d9ddb9cbae6c6fd62ee855a37cd20a9762d0e
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-08-06 14:15:11 +0530

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
       <body className={inter.className}>{children}
+        <Analytics />
       <Toaster />
       </body>
     </html>
+    
   );
 }
